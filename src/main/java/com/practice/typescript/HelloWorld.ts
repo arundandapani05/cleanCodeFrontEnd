2608c04a5f7ae72456cad0fc915c335f26153d6d
m3nqqs <arun.dandapani@siemens.com>
2020-05-17 13:23:12 +0530

    HelloWorld program for typescript

---
+++ src/main/java/com/practice/typescript/HelloWorld.ts
@@ -0,0 +1,6 @@
+function f() {
+    var message:string = "Hello World"
+    console.log(message)
+    document.getElementById("demo").innerHTML=message;
+    return "Hello This example is proof that this works";
+}
