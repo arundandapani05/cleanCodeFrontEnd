623f53e738cbe7782487e93c78f3739afbd42a93
m3nqqs <arun.dandapani@siemens.com>
2020-05-18 09:45:24 +0530

    understanding typescript

--- src/main/java/com/practice/typescript/HelloWorld.ts
+++ src/main/java/com/practice/typescript/HelloWorld.ts
@@ -4,3 +4,15 @@ function f() {
     document.getElementById("demo").innerHTML=message;
     return "Hello This example is proof that this works";
 }
+function outerFunction(arg) {
+    var variableInOuterFunction = arg;
+
+    function bar() {
+        console.log(variableInOuterFunction); // Access a variable from the outer scope
+    }
+
+    // Call the local function to demonstrate that it has access to arg
+    bar();
+}
+
+outerFunction("hello closure"); // logs hello closure!
